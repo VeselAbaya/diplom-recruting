ff101852a4b5e18c03476367d8f1041a2c3dd996
VeselAbaya <vandervise465@gmail.com>
2021-11-12 10:50:29 +0300

    #24 Add new field to IUserListDto#relationsWithOriginCount

--- shared/types/user/user-list-item.dto.interface.ts
+++ shared/types/user/user-list-item.dto.interface.ts
@@ -3,6 +3,7 @@ import { IUserDto } from './user.dto.interface';
 export type IUserListItem = IUserDto & {
   notifications: number,
   relationsCount: number,
+  relationsWithOriginCount: number,
   networkSize: number,
   intermediate: boolean
 };
