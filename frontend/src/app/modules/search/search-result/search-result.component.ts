14f1df8529b263f0ce19524ef44ea455e7356417
Antay Juskovets <vandervise465@gmail.com>
2021-12-04 12:11:59 +0300

    Closes #67, closes #66 (refactoring) SearchModule

--- frontend/src/app/modules/search/search-result/search-result.component.ts
+++ frontend/src/app/modules/search/search-result/search-result.component.ts
@@ -1,46 +1,55 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { map, switchMap } from 'rxjs/operators';
+import { debounceTime, map, switchMap } from 'rxjs/operators';
 import { SearchService } from '@modules/search/search.service';
 import { PageEvent } from '@angular/material/paginator';
-import { LIMITS } from '@monorepo/types/pagination/limits';
 import { RelationsService } from '../relations.service';
 import { ProfileService } from '@modules/profile/profile.service';
 import { SearchParamsService } from '@modules/search/search-params/search-params.service';
+import { OnDestroyMixin, untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
+import { combineLatest } from 'rxjs';
 
-// TODO SearchResultList and SearchResultGraph accepts user data in different ways
-//      *SearchResultGraph fetching it by itself
-//      *SearchResultList accepts it via @Input
-//      IT SEEMS LIKE SHIT
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchResultComponent {
+export class SearchResultComponent extends OnDestroyMixin {
   @Input() withGraphView = false;
 
-  usersList$ = this.profile.selectedUser$.pipe(
+  readonly usersList$ = this.profile.selectedUser$.pipe(
     switchMap(selectedUser => selectedUser !== null
       ? this.relations.result$.pipe(map(graph => graph?.nodes.filter(user => user.id !== selectedUser.id)))
       : this.search.result$
     )
   );
 
-  isLoading$ = this.profile.selectedUser$.pipe(
-    switchMap(selectedUser => selectedUser !== null ? this.relations.isLoading$ : this.search.usersLoading$)
+  readonly isLoading$ = this.profile.selectedUser$.pipe(
+    switchMap(selectedUser => selectedUser !== null
+      ? this.relations.isLoading$
+      : this.search.usersLoading$
+    )
   );
-
-  // TODO check if we can configure material paginator to default limits
-  LIMITS = LIMITS;
 
   constructor(readonly search: SearchService,
               readonly relations: RelationsService,
               readonly profile: ProfileService,
               private readonly searchParams: SearchParamsService) {
+    super();
+    combineLatest([
+      this.searchParams.params$,
+      this.profile.selectedUser$
+    ]).pipe(
+      debounceTime(0),
+      untilComponentDestroyed(this),
+      switchMap(([params, selectedUsed]) => selectedUsed !== null
+        ? this.relations.getGraph({ ...params, fromUserId: selectedUsed.id })
+        : this.search.getUsers(params)
+      )
+    ).subscribe();
   }
 
-  onPageChange({pageIndex: page, pageSize: limit}: PageEvent): void {
-    this.searchParams.patch({page, limit});
+  onPageChange({ pageIndex: page, pageSize: limit }: PageEvent): void {
+    this.searchParams.patch({ page, limit });
   }
 }
--- frontend/src/app/modules/search/search.component.ts
+++ frontend/src/app/modules/search/search.component.ts
@@ -1,15 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderService } from '@modules/header/header.service';
 import { MessagesService } from '@shared/components/messages/messages.service';
-import { debounceTime, distinctUntilChanged, filter, map, startWith, switchMap, take, tap } from 'rxjs/operators';
-import { combineLatest, forkJoin, of } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { SearchService } from '@modules/search/search.service';
 import { OnDestroyMixin, untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
 import { NavigationEnd, Router } from '@angular/router';
-import { FormControl } from '@angular/forms';
-import { equals, uncurryN } from 'ramda';
-import { DEFAULT_SEARCH_PARAMS } from '@modules/search/search-params/default-search-params';
-import { SearchParamsService } from '@modules/search/search-params/search-params.service';
 import { ProfileService } from '@modules/profile/profile.service';
 
 @Component({
@@ -18,49 +13,16 @@ import { ProfileService } from '@modules/profile/profile.service';
   styleUrls: ['./search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchComponent extends OnDestroyMixin implements OnInit {
-  readonly searchForm = new FormControl();
-
+export class SearchComponent extends OnDestroyMixin {
   constructor(readonly messages: MessagesService,
               readonly header: HeaderService,
               readonly router: Router,
               readonly search: SearchService,
-              readonly searchParams: SearchParamsService,
               readonly profile: ProfileService) {
     super();
-    this.searchForm.setValue(this.searchParams.get());
     router.events.pipe(
       untilComponentDestroyed(this),
       filter(e => e instanceof NavigationEnd && router.url === '/search')
     ).subscribe(() => header.setTitle('Search'));
-
-    const debouncedValidSearchFormValue$ = this.searchForm.valueChanges.pipe(
-      debounceTime(500),
-      filter(() => this.searchForm.valid),
-      distinctUntilChanged(uncurryN(2, equals)),
-      tap(formValue => this.searchParams.patch(formValue)),
-      startWith(this.searchForm.value)
-    );
-
-    const selectedUser$ = this.profile.selectedUser$.pipe(
-      map(user => user?.id ?? DEFAULT_SEARCH_PARAMS.fromUserId)
-    );
-
-    combineLatest([debouncedValidSearchFormValue$, selectedUser$]).pipe(
-      untilComponentDestroyed(this),
-      switchMap(([formValue, fromUserId]) => forkJoin([
-        of(formValue),
-        of(fromUserId),
-        this.search.pagination$.pipe(take(1))
-      ])),
-      map(([formValue, fromUserId, {page, limit}]) => ({
-        ...formValue, page, limit, fromUserId
-      })),
-      switchMap(newParams => this.search.getUsers(newParams))
-    ).subscribe();
-  }
-
-  ngOnInit(): void {
-
   }
 }
