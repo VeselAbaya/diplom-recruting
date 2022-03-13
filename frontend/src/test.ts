30bb8045dc5fee198c2b47b3dd49e96b8247bb93
Antay Juskovets <31548180+VeselAbaya@users.noreply.github.com>
2022-03-13 19:58:33 +0300

    Closes #48 (refactoring) Migrate angular from 12 -> 13 (with material and cdk) (#96)
    * (refactoring) update @angular/core and cli
    
    * Closes #48 (refactoring) Update @angular/material and cdk

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -2,10 +2,7 @@
 
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
@@ -17,7 +14,9 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: false }
+  }
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
